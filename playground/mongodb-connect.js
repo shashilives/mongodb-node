80ffe08501f6fcda96eab47879a644d09e97e2c9
Shashi Kumar <shashikumarlives@gmail.com>
2018-06-11 13:45:40 +0530

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,24 @@
+const MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://127.0.0.1:27017";
+// url shd start with mongodb / server:port / database name
+var url = "mongodb://localhost:27017/TodoApp";
+MongoClient.connect(url, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDb server');
+    }
+    console.log('Connected to MongoDb server');
+    const db = client.db('TodoApp');
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, res) => {
+        if (err) {
+            return console.log('unable to insert todo', err);
+        }
+
+        console.log(JSON.stringify(res.ops, undefined, 2));
+
+    });
+
+    client.close(); //closes the connection with mongodb
+});

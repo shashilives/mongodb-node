8c9846ce95404a47b5879997ae194b665c509a80
Shashi Kumar <shashikumarlives@gmail.com>
2018-06-16 11:44:08 +0530

    ADD get todos route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,17 @@ app.post('/todos', (req, res) => {
 });
 app.get('/todos', (req, res) => {
 
+    Todo.find().then((todos) => {
+        res.send({
+            todos: todos
+        });
+    }, (err) => {
+        res.status(400).send({
+            error: err
+        });
+    })
+
+
 });
 app.get('/todos/:id', (req, res) => {
 
